8cf3f8a7c4751e17b99801646cd1ca61a655507e
Paul Morganthall <paul@morganthall.com>
2011-02-17 14:24:13 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doctorcase::Application.config.secret_token = 'bb78677ea0232acadb758813df0c7e7fbd4860d0969fdf80517a37952f0b638f344e96aec321eae5abb70a1a6367a30622102b2e85472815ad081cedbef1af07'
